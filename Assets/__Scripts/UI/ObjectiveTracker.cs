a5bc6405bfd1ab51788451e6ed9c6dd8631a7be7
Brendan Connolly <bconnoll@uccs.edu>
2023-06-18 15:25:33 -0600

    Objective Tracker UI Improvements

--- Assets/__Scripts/UI/ObjectiveTracker.cs
+++ Assets/__Scripts/UI/ObjectiveTracker.cs
@@ -9,22 +9,43 @@ public class ObjectiveTracker : MonoBehaviour
 
     private List<Objective> objectives;
 
-    [SerializeField] private TextMeshProUGUI objectiveText;
+    [SerializeField] private GameObject textPrefab;
+    private TextMeshProUGUI[] objectiveText;
 
     // Start is called before the first frame update
     void Start()
     {
         objectives = GameManager.Instance.Objectives;
+        objectiveText = new TextMeshProUGUI[objectives.Count];
+        for (int i = 0; i < objectives.Count; i++)
+        {
+            GameObject go = Instantiate(textPrefab, transform);
+            objectiveText[i] = go.GetComponent<TextMeshProUGUI>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        string t = "";
-        foreach(Objective o in objectives)
+        SetText();
+    }
+
+    void SetText()
+    {
+        for(int i = 0; i < objectives.Count; i++)
         {
-            t += $"{o.Name}\n";
+            SetFont(i);
+            objectiveText[i].text = $"{objectives[i].Name}\n";
         }
-        objectiveText.text = t;
+    }
+
+    void SetFont(int i)
+    {
+        if (objectives[i].IsComplete)
+        {
+            objectiveText[i].color = Color.green;
+            objectiveText[i].fontStyle = FontStyles.Strikethrough;
+
+        }
     }
 }
